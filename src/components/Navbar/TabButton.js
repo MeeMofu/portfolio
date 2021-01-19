7c8c789e19f08029edcd1aec2203c80dc3291b47
Vinh Tran <13362866+MeeMofu@users.noreply.github.com>
2021-01-19 00:39:37 -0500

    Minor adjustment to navigation buttons

--- src/components/Navbar/TabButton.js
+++ src/components/Navbar/TabButton.js
@@ -6,29 +6,32 @@ function TabBtn(props) {
     const [isActive,setActive] = useState(false);
 
     useEffect(()=>{
+        // Update underline state whenever tab change
         (thisTab === currentTab)? setActive(true):setActive(false);
-    },[currentTab]);
+    },[thisTab,currentTab]);
 
     return (
-        <div className="tabBtn text px-2 pb-2"
+        <div className={`tabBtn text px-3 pb-2 ${currentTab.name === thisTab.name && 'tabActive'}`}
             onMouseEnter={() => {
                 if (thisTab !== currentTab) setActive(true);
+                // show line when hover and notactive
             }}
             onMouseLeave={() => {
                 if (thisTab !== currentTab) setActive(false);
+                // hide line when leave and not active
             }}
             onClick ={() => {
+                // set active and update current tab
                 setActive(true)
                 setTab(thisTab)
             }}>
             
-            {thisTab.name}
+                {thisTab.name}
 
             <Animated animationIn="zoomIn" animationOut="zoomOut" 
                 animationInDuration={500} animationOutDuration={500} 
-                isVisible={isActive} 
-                
-                >
+                isVisible={isActive}>
+                    {/* A line that's animated to fade in and out */}
                     <div className="hl"></div>
             </Animated>
             
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import TabBtn from './TabButton';
 
 function Nav(props) {
@@ -7,7 +7,7 @@ function Nav(props) {
     return (
         <nav className="row no-gutters col-12 header">
             <h1 className="col-12 pb-2 pl-4 pt-3 m-0 text text-center text-sm-left">Vinh Tran</h1>
-            <div className="col-12 pb-2 btn-group  d-flex justify-content-between justify-content-sm-end" >
+            <div className="col-12 pb-2 btn-group  d-flex justify-content-center justify-content-sm-end" >
                 {tabs.map((tab)=>(
                     <TabBtn thisTab= {tab} currentTab={currentTab} setTab={setTab}
                     key ={tab.name} />
